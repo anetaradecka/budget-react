7400a6a5fb59ef6e3b2f962d70af056f9b5fd731
anetaradecka <aneta.radecka@outlook.com>
2023-12-22 12:53:36 +0100

    displying page title on header

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -4,18 +4,35 @@ import {
   useSubmit,
   useLocation,
 } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./../components/layout/Header/Header";
 import Sidebar from "./../components/layout/Sidebar/Sidebar";
 import Main from "./../components/layout/Main/Main";
 import Dashboard from "./Dashboard/Dashboard";
 import { getTokenDuration } from "../util/auth";
+import { TitleContext } from "../contex/title-context";
 
 function RootLayout() {
   const location = useLocation();
   const { pathname } = location;
   const token = useLoaderData();
   const submit = useSubmit();
+  const [pageTitle, setTitle] = useState("Dashboard");
+
+  useEffect(() => {
+    let pageTitle;
+    const extractedTitle = pathname.slice(5);
+    switch (extractedTitle) {
+      case "":
+        pageTitle = "Dashboard";
+        break;
+      case "transactions":
+        pageTitle = "Transactions";
+        break;
+      default:
+    }
+    setTitle(pageTitle);
+  }, [pathname]);
 
   useEffect(() => {
     if (!token) {
@@ -35,9 +52,11 @@ function RootLayout() {
 
   return (
     <>
-      <Header />
-      <Sidebar />
-      <Main>{pathname === "/app" ? <Dashboard /> : <Outlet />}</Main>
+      <TitleContext.Provider value={{ pageTitle: pageTitle }}>
+        <Header />
+        <Sidebar />
+        <Main>{pathname === "/app" ? <Dashboard /> : <Outlet />}</Main>
+      </TitleContext.Provider>
     </>
   );
 }
