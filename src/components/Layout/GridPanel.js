7cc68682746a2b4740da754a7a59d6beed4d57d4
anetaradecka <aneta.radecka@outlook.com>
2024-06-29 19:08:31 +0200

    grid row amended with img and edit and delete buttons

--- src/components/Layout/GridPanel.js
+++ src/components/Layout/GridPanel.js
@@ -1,141 +1,12 @@
-import { useState, useRef, useEffect } from "react";
-// import { redirect } from "react-router-dom";
-// import { getAuthToken } from "../../utils/auth";
-// import { getCSRFToken } from "../../utils/auth";
-
+// external libraries
+import { useRef } from "react";
+// styles
 import styles from "./TransactionsGrid.module.css";
-
+// components
 import GridRow from "./GridRow";
-import Img from "./Img";
-import Edit from "./Edit";
-import Delete from "./Delete";
-import DataRow from "./DataRow";
-// import Container from "../../components/Containers/Container/Container";
-// import AddTransaction from "./AddTransaction";
 
 const GridPanel = (props) => {
   const scrollRef = useRef(null);
-  //   const [transactions, setTransactions] = useState([]);
-
-  //   useEffect(() => {
-  //     async function fetchTransactions() {
-  //       const token = getAuthToken();
-  //       const csrfToken = getCSRFToken();
-
-  //       const response = await fetch(
-  //         `http://localhost:8080/transactions?page=1`,
-  //         {
-  //           headers: {
-  //             // method: "GET", // Why blocked by CORS?
-  //             Authorization: "Bearer " + token,
-  //             "X-CSRF-Token": csrfToken,
-  //           },
-  //         }
-  //       );
-
-  //       if (!response.ok) {
-  //         throw new Response(
-  //           JSON.stringify({ message: "Could not fetch transactions" }),
-  //           { status: 500 }
-  //         );
-  //       }
-
-  //       const data = await response.json();
-  //       setTransactions(data.transactions);
-  //     }
-
-  //     fetchTransactions();
-  //   }, []);
-
-  //   const handleItemDelete = async (itemId) => {
-  //     const filteredItems = transactions.filter((item) => {
-  //       return item._id !== itemId;
-  //     });
-
-  //     setTransactions(filteredItems);
-
-  //     const token = getAuthToken();
-  //     const csrfToken = getCSRFToken();
-
-  //     const response = await fetch(
-  //       "http://localhost:8080/transactions/" + itemId,
-  //       {
-  //         method: "delete",
-  //         headers: {
-  //           Authorization: "Bearer " + token,
-  //           "X-CSRF-Token": csrfToken,
-  //         },
-  //       }
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Response(
-  //         JSON.stringify({ message: "Could not delete transaction" }),
-  //         { status: 500 }
-  //       );
-  //     }
-  //   };
-
-  //   const handleTransactionSubmit = async (data) => {
-  //     const submitData = {
-  //       category: data.category,
-  //       date: data.date,
-  //       value: data.value,
-  //       description: data.description,
-  //       type: data.type,
-  //     };
-
-  //     const token = getAuthToken();
-  //     const csrfToken = getCSRFToken();
-
-  //     const response = await fetch(
-  //       "http://localhost:8080/transactions/add-transaction",
-  //       {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: "Bearer " + token,
-  //           "X-CSRF-Token": csrfToken,
-  //         },
-  //         body: JSON.stringify({ submitData }),
-  //       }
-  //     );
-
-  //     if (!response.ok) {
-  //       throw new Response(
-  //         JSON.stringify({ message: "Could not add transaction" }),
-  //         { status: 500 }
-  //       );
-  //     }
-
-  //     async function fetchTransactions() {
-  //       const token = getAuthToken();
-  //       const csrfToken = getCSRFToken();
-
-  //       const response = await fetch(
-  //         `http://localhost:8080/transactions?page=1`,
-  //         {
-  //           headers: {
-  //             // method: "GET", // Why blocked by CORS?
-  //             Authorization: "Bearer " + token,
-  //             "X-CSRF-Token": csrfToken,
-  //           },
-  //         }
-  //       );
-
-  //       if (!response.ok) {
-  //         throw new Response(
-  //           JSON.stringify({ message: "Could not fetch transactions" }),
-  //           { status: 500 }
-  //         );
-  //       }
-
-  //       const data = await response.json();
-  //       setTransactions(data.transactions);
-  //     }
-
-  //     fetchTransactions();
-  //   };
 
   return (
     <div className={styles["grid-panel"]}>
@@ -158,28 +29,12 @@ const GridPanel = (props) => {
           )}
           {props.transactions.length > 0 &&
             props.transactions.map((transaction) => (
-              <GridRow key={transaction._id}>
-                <Img />
-                <DataRow transaction={transaction} />
-                <div
-                  className={`${styles["cell-edit"]} ${styles["grid-cell"]}`}
-                >
-                  <Edit itemId={transaction._id} />
-                  <Delete
-                    itemId={transaction._id}
-                    onItemDelete={props.onDelete}
-                  />
-                </div>
-              </GridRow>
+              <GridRow key={transaction._id} transaction={transaction} />
             ))}
         </div>
       </section>
     </div>
   );
 };
-
-// export const addTransactionAction = async ({ request }) => {
-//   return redirect("/app/transactions");
-// };
 
 export default GridPanel;
--- src/components/Layout/GridRow.js
+++ src/components/Layout/GridRow.js
@@ -1,7 +1,44 @@
+// styles
 import styles from "./TransactionsGrid.module.css";
+// components
+import Img from "./Img";
+import Edit from "./Edit";
+import Delete from "./Delete";
+// utils
+import { currencyFormatter, convertToLocaleDate } from "../../utils/formatting";
 
 const GridRow = (props) => {
-  return <div className={styles["grid-row"]}>{props.children}</div>;
+  return (
+    <div className={styles["grid-row"]}>
+      <Img />
+      <div className={`${styles["grid-cell"]} ${styles["data-item"]}`}>
+        {props.transaction.description}
+      </div>
+      <div className={`${styles["grid-cell"]} ${styles["data-item"]}`}>
+        {props.transaction.category}
+      </div>
+      <div className={`${styles["grid-cell"]} ${styles["data-item"]}`}>
+        {props.transaction.subcategory}
+      </div>
+      <div className={`${styles["grid-cell"]} ${styles["data-item"]}`}>
+        {String(convertToLocaleDate(props.transaction.date))}
+      </div>
+      <div className={`${styles["grid-cell"]} ${styles["data-item"]}`}>
+        {props.transaction.type === "inflow"
+          ? `+ ${currencyFormatter.format(props.transaction.value)} `
+          : ""}
+      </div>
+      <div className={`${styles["grid-cell"]} ${styles["data-item"]}`}>
+        {props.transaction.type === "outflow"
+          ? `- ${currencyFormatter.format(props.transaction.value)} `
+          : ""}
+      </div>
+      <div className={`${styles["cell-edit"]} ${styles["grid-cell"]}`}>
+        <Edit itemId={props.transaction._id} />
+        <Delete itemId={props.transaction._id} onItemDelete={props.onDelete} />
+      </div>
+    </div>
+  );
 };
 
 export default GridRow;
