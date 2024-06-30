6000b241b212245407f0b91695d3b597172f24fa
anetaradecka <aneta.radecka@outlook.com>
2024-06-30 18:01:49 +0200

    fetch transactions moved to utils

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,77 @@
-export async function fetchData() {
-  // przeniesc fetcha
+import { getAuthToken, getCSRFToken } from "./auth";
+
+export async function fetchTransactionsList() {
+  const token = getAuthToken();
+  const csrfToken = getCSRFToken();
+
+  const response = await fetch(`http://localhost:8080/transactions?page=1`, {
+    headers: {
+      // method: "GET", // Why blocked by CORS?
+      Authorization: "Bearer " + token,
+      "X-CSRF-Token": csrfToken,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not fetch transactions" }),
+      { status: 500 }
+    );
+  }
+
+  const data = await response.json();
+  return data.transactions;
+}
+
+export async function deleteOneItem(itemId) {
+  const token = getAuthToken();
+  const csrfToken = getCSRFToken();
+
+  const response = await fetch("http://localhost:8080/transactions/" + itemId, {
+    method: "delete",
+    headers: {
+      Authorization: "Bearer " + token,
+      "X-CSRF-Token": csrfToken,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not delete transaction" }),
+      { status: 500 }
+    );
+  }
+}
+
+export async function submitNewItem(data) {
+  const token = getAuthToken();
+  const csrfToken = getCSRFToken();
+
+  const payload = {
+    category: data.category,
+    date: data.date,
+    value: data.value,
+    description: data.description,
+    type: data.type,
+  };
+
+  const response = await fetch(
+    "http://localhost:8080/transactions/add-transaction",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+        "X-CSRF-Token": csrfToken,
+      },
+      body: JSON.stringify({ payload }),
+    }
+  );
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Could not add transaction" }),
+      { status: 500 }
+    );
+  }
 }
